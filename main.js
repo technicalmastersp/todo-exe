1eedc0dcc8fbbc9012af00e0d1338cc912aba4f5
technicalmastersp <takebestservices@gmail.com>
2025-01-10 02:53:08 +0530

    added input style and min max size

--- main.js
+++ main.js
@@ -1,21 +1,34 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
 
+let win;
 function createWindow() {
-  const win = new BrowserWindow({
-    width: 300,
-    height: 400,
-
+  win = new BrowserWindow({
+    minWidth: 260,
+    width: 280,
+    height: 320,
+    maxWidth: 350,
+    maxHeight: 450,
     webPreferences: {
       nodeIntegration: true,
     },
     alwaysOnTop: true,
     focusable: true,
-    //     transparent: true, // Make the window transparent
-    //     frame: false, // Optional: Remove the window frame to make it fully transparent
+    resizable: true,
+    maximizable: false,
+    icon: path.join(__dirname, "icon.png"),
   });
 
   win.loadFile("index.html");
+
+  win.on("focus", () => {
+    win.setOpacity(1); //When the app is focused, make it fully visible (100%)
+    win.setSize(win.getSize()[0], 300);
+  });
+  win.on("blur", () => {
+    win.setOpacity(0.3); //When the app is not focused, make it semi-transparent (65%)
+    win.setSize(win.getSize()[0], 0);
+  });
 }
 
 app.whenReady().then(createWindow);
